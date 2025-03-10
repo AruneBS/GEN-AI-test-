fffd9f759df2768473e33bde8b52c3e1665ff61e
Arūnė Bražiūnaitė <117632336+AruneBS@users.noreply.github.com>
2025-03-10 17:30:55 +0200

    add message

---
+++ openai-client/openai_client09.py
@@ -0,0 +1,32 @@
+import os
+from openai import OpenAI
+
+from dotenv import load_dotenv
+from rich import print 
+
+load_dotenv()
+
+
+client = OpenAI(
+    base_url="https://models.inference.ai.azure.com",
+    api_key=os.environ["GITHUB_TOKEN"],
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "",
+        },
+        {
+            "role": "user",
+            "content": "What is the capital of France?",
+        }
+    ],
+    model="gpt-4o",
+    temperature=1,
+    max_tokens=4096,
+    top_p=1
+)
+
+print(response.choices[0].message.content)
