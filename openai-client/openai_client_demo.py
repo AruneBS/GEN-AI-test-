8d46280154d5514d2c9aa7b5f5e00095e774aa55
Arūnė Bražiūnaitė <117632336+AruneBS@users.noreply.github.com>
2025-03-12 22:40:53 +0200

    Add message

--- openai-client/openai_client_demo.py
+++ openai-client/openai_client_demo.py
@@ -1,21 +1,9 @@
 import os
-from openai import OpenAI
 
 from dotenv import load_dotenv
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),  # This is the default and can be omitted
-)
+api_key = os.getenv('GITHUB_TOKEN')
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-4o",
-)
-print(chat_completion)
+print(api_key)
 
